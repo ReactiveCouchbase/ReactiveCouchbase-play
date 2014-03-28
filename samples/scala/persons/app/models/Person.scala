b85a7ad04c5fc95027279f26b0be2adca93367f9
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2014-03-28 22:27:44 +0100

    Fix sample

--- samples/scala/persons/app/models/Person.scala
+++ samples/scala/persons/app/models/Person.scala
@@ -9,27 +9,4 @@ case class Person(name: String, surname: String, age: Int)
 
 object Persons {
   implicit val fmt = Json.format[Person]
-
-  def read(key: String): Future[JsResult[JsValue]] = ???
-  def update(doc: JsValue): Future[JsResult[JsValue]] = ???
-
-  def readAndUpdate(id: String, block : (JsValue) => JsValue): Future[JsResult[JsValue]] = {
-    read(id).flatMap {
-      case JsSuccess(document, _) => update(block(document))
-      case error: JsError => Future.successful(error)
-    }
-  }
-
-  def readAndUpdate2(id: String, block : (JsValue) => JsValue ) : Future[JsResult[JsValue]] = {
-    read(id).map( _ match {
-      case JsSuccess(document, _) => {
-        val documentToUpdate = block(document)
-        Await.result(update(documentToUpdate), Duration(30, TimeUnit.MICROSECONDS))
-      }
-      case error@JsError(_) => error
-    }
-    )
-
-    Future.f("o")
-  }
 }

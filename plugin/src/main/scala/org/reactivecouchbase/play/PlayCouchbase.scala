51ee954e0b82e2ebf209ba846870e122fdb7c9fa
Andreas Teichrib <andreas@teichrib.de>
2014-09-26 15:22:08 +0200

    The `couchbaseActorSystem` should use the same configuration path as the `defaultActorSystem` in the ReactiveCouchbaseDriver

--- plugin/src/main/scala/org/reactivecouchbase/play/PlayCouchbase.scala
+++ plugin/src/main/scala/org/reactivecouchbase/play/PlayCouchbase.scala
@@ -4,7 +4,7 @@ import com.couchbase.client.CouchbaseClient
 import play.api._
 import scala.concurrent.ExecutionContext
 import akka.actor.ActorSystem
-import org.reactivecouchbase.{Couchbase, RCLogger, LoggerLike, CouchbaseBucket}
+import org.reactivecouchbase.{RCLogger, LoggerLike, CouchbaseBucket}
 import org.reactivecouchbase.play.plugins.CouchbasePlugin
 import org.reactivecouchbase.experimental.CappedBucket
 
@@ -15,7 +15,7 @@ object PlayCouchbase {
   private[reactivecouchbase] val initMessage = "The CouchbasePlugin has not been initialized! Please edit your conf/play.plugins file and add the following line: '400:package org.reactivecouhbase.play.CouchbasePlugin' (400 is an arbitrary priority and may be changed to match your needs)."
   private[reactivecouchbase] val connectMessage = "The CouchbasePlugin doesn't seems to be connected to a Couchbase server. Maybe an error occured!"
 
-  private[reactivecouchbase] val couchbaseActorSystem = ActorSystem("reactivecouchbase-plugin-system")
+  private[reactivecouchbase] val maybeCouchbaseActorSystem: Option[ActorSystem] = None
   private[reactivecouchbase] val loggerFacade = PlayLogger.logger("ReactiveCouchbase")
 
   def defaultBucket(implicit app: Application): CouchbaseBucket = app.plugin[CouchbasePlugin] match {
@@ -48,6 +48,10 @@ object PlayCouchbase {
           throw new PlayException("Configuration issue", "You have to define a 'couchbase.execution-context.execution-context' object in the application.conf file.")
       }
     }
+  }
+
+  def couchbaseActorSystem(implicit app: Application) = {
+    maybeCouchbaseActorSystem.getOrElse(ActorSystem("reactivecouchbase-plugin-system", Play.configuration(app).underlying.getConfig("couchbase.actorctx")))
   }
 }
 
